3b2d8d951d182b16af3165da5d95f68290ae5b3d
MD Millat Hosen <codermillat@gmail.com>
2024-09-23 19:13:43 +0000

    First day in JavaScript

---
+++ Basics_JS/01_vabirables.js
@@ -0,0 +1,8 @@
+const accountID = 192
+let email = "contact@millat.me"
+var acPassword = "12342"
+accountCity = "Dinajpur"
+
+// accountID = 5
+console.log(accountID)
+console.table([accountID, acPassword, email, accountCity])
