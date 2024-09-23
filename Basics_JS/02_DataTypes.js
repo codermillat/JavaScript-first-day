5ef425b2d15e8f1b28e52dcbf3bd4a5f9eb770c7
MD Millat Hosen <codermillat@gmail.com>
2024-09-23 19:52:24 +0000

    DataTypes

--- Basics_JS/02_DataTypes.js
+++ Basics_JS/02_DataTypes.js
@@ -4,9 +4,9 @@
 
 console.log(3 + 3); // code readability should be high
 
-console.log("Hitesh");
+console.log("MILLAT");
 
-let name = "hitesh";
+let name = "MILLAT";
 let age = 18;
 let isLoggedIn = false;
 let state;
